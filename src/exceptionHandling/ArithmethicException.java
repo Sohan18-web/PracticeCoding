4d346d4a7c0e29667343fd416330d9e27b33256a
Sohan18-web <sohanraut903@gmail.com>
2025-05-26 20:43:28 +0530

    Added clean and close the Scanner with the use of Try resources

--- src/exceptionHandling/ArithmethicException.java
+++ src/exceptionHandling/ArithmethicException.java
@@ -7,19 +7,16 @@ public class ArithmethicException {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner scan =new Scanner(System.in);
-		int n=scan.nextInt();
-		
-		System.out.println("Dividing the Number by 0");
-		try {
-			System.out.println(n/0);
-			
+		try (Scanner scan = new Scanner(System.in)) {
+			int n = scan.nextInt();
+			System.out.println("Dividing the Number by 0");
+			System.out.println(n / 0);
+
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		catch(Exception e) {
-		  	System.out.println(e);
-		}
+
 		System.out.println("Program Completed");
 	}
 
 }
- 
